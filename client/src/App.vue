d90714350d063f171992aa3f0acd44947a6d6575
Richard <fahitch+github@gmail.com>
2019-12-27 17:51:21 +0000

    front page logo changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,34 +9,34 @@
 
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="FRNG Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="/FRNG_logo.svg"
           transition="scale-transition"
-          width="40"
+          width="120"
         />
 
-        <v-img
+        <!-- <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
+      <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-content>
--- client/src/components/WelcomePage.vue
+++ client/src/components/WelcomePage.vue
@@ -46,7 +46,7 @@
         >
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
+        IF YOU WNATED
           <br>please join our online
           <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
         </p>
