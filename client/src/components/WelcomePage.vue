e8d4468c2bd7cf3dcf60279d8b1fcd1cb3f68727
Richard <fahitch+github@gmail.com>
2019-12-27 16:36:10 +0000

    add server

---
+++ client/src/components/WelcomePage.vue
@@ -0,0 +1,173 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <!-- <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex> -->
+
+      <v-flex mb-4 pa-4>
+        <h1 class="display-2 font-weight-bold mb-2">
+          Welcome to Fringe Coin
+        </h1>
+      </v-flex>
+
+      <v-flex ma-4 pa-4
+        d-flex
+      >
+        <v-card >
+          <v-card-title
+          ma-2>£0.00</v-card-title>
+          <v-card-subtitle>Account Total</v-card-subtitle>
+          <v-card-text>This is the amount deposited by you into your account.</v-card-text>
+          <v-card-actions><a href="#account">View Account</a></v-card-actions>
+        </v-card>
+
+        <v-card
+          ma-2
+          pa-6
+          color="success"
+          disabled
+        >
+          <v-card-title>
+          Global Info </v-card-title>
+          <v-card-text>This is where a graph expressing some sort of global information of the coin would be displayed.
+              <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </v-card-text>
+        </v-card >
+        <v-card
+        >
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </p>
+        </v-card >
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'WelcomePage',
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
+};
+</script>
